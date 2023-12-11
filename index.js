89b3305208de86c7437b0d1fea3f8bc1d4d1ba49
Charlie Schubach <charlie.schubach@gmail.com>
2023-12-11 12:46:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(str = 'roller-skate') {
+    return `This Saturday, I want to ${str}!`
+}
+
+function mondayWork(str = 'go to the office') {
+    return `This Monday, I will ${str}.`
+}
+
+function wrapAdjective(char) {
+    return function callback(str) {
+      return `You are ${char}${str}${char}!`
+    }
+}
